5d1a8a273979992b5850b20bdfec4bfd39d7530f
Bogdan <cormack567@mail.ru>
2025-12-03 14:56:41 +0300

    testing implementations openzeppelin and solady

---
+++ contracts/ERCOpenZeppelin.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken2 is ERC20{
+  string public _name;
+  string public _symbol;
+
+  constructor() ERC20(_name, _symbol) {
+        _name = "MyToken";
+        _symbol = "MT";
+  }
+
+      function name() public view override returns (string memory){
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory){
+        return _symbol;
+    }
+
+  function mintPublic(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burnPublic(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
---
+++ contracts/ERCSoladyMock.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "solady/src/tokens/ERC20.sol";
+
+contract MyToken3Mock is ERC20{
+    string public _name;
+    string public _symbol;
+
+    constructor(){
+        _name = "MyToken";
+        _symbol = "MT";
+    }
+
+      function mintPublic(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burnPublic(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+
+    function name() public view override returns (string memory){
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory){
+        return _symbol;
+    }
+}
