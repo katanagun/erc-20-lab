4e867705cdf1df0ad65ab8dd3b51c10a32ebbb7b
Bogdan <cormack567@mail.ru>
2026-05-14 15:20:40 +0300

    new version

--- contracts/ERCSolady.sol
+++ contracts/ERCSolady.sol
@@ -4,13 +4,6 @@ pragma solidity ^0.8.4;
 import "solady/src/tokens/ERC20.sol";
 
 contract Solady is ERC20{
-     //keccak256("MyToken")
-    bytes32 internal constant _NAME_HASH =
-        0x245c734e6d4ec044daf7beffa09d54d4bafba490113c199734d790b04a7390e5;
-
-    // keccak256("1")
-    bytes32 internal constant _VERSION_HASH =
-        0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6;
 
     function name() public pure override returns (string memory) {
         return "MyToken";
@@ -18,14 +11,6 @@ contract Solady is ERC20{
 
     function symbol() public pure override returns (string memory) {
         return "MT";
-    }
-
-    function _constantNameHash() internal pure override returns (bytes32) {
-        return _NAME_HASH;
-    }
-
-    function _versionHash() internal pure override returns (bytes32) {
-        return _VERSION_HASH;
     }
 
       function mintPublic(address to, uint256 amount) external {
